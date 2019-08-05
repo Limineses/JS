77b9f59fed0defdd2cf06dc2972cfb394622a38a
Adrew Hod <andreihodunov3@tut.by>
2019-08-05 22:04:05 +0300

    Lesson 4(2)

--- Lesson 4/Regular expression/script.js
+++ Lesson 4/Regular expression/script.js
@@ -1,13 +1,13 @@
-var str = "344-85-68 <a>www.google.com</a> <b>ul.Filevskaya, d.13</b>  962-34-49 ul.Lybanskaya, d.67  8 897-30-42 www.yandex.by"
+var str = "344-85-68 <a> http(s)://www.google.com</a> <b>ul.Filevskaya, d.13</b>  962-34-49 ul.Lybanskaya, d.67  8 897-30-42 www.yandex.by random.si-te.com my-email@gmail.com; To: company22@c.org; Copy: alpha@b.de "
 
 var phoneNumbers = /\d{3}\-\d{2}\-\d{2}/g;
 console.log(str.match(phoneNumbers));
 
-var webSite = /w{3}\.\w+\.\w+/g;
+var webSite = /[a-z][a-z\-\_\.]+[a-z]\.[a-z]{2,10}/g;
 console.log(str.match(webSite));
 
 var address = /ul\.\w+\,\sd\.\d+/gi;
 console.log(str.match(address));
 
-var html = /\<\/?\w+\>/gi;
+var html = /\<\/?[a-z]+\>/gi;
 console.log(str.match(html));
--- Lesson 4/Scroll page/script.js
+++ Lesson 4/Scroll page/script.js
@@ -1,8 +1,8 @@
 var scrollPage = setInterval(function()
 {
-    scrollBy(0, 1);
+    scrollBy(0, 5);
     if(scrollY == document.documentElement.scrollHeight - window.innerHeight)
     {
         clearInterval(scrollPage);        
     }
-}, 1);
+}, 20);
