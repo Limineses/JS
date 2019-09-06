7c4b990f832cae3e1bf945669e204293419453b7
Adrew Hod <andreihodunov3@tut.by>
2019-09-06 16:43:18 +0300

    Lesson 11(2)

--- Lesson 11/script.js
+++ Lesson 11/script.js
@@ -1,4 +1,4 @@
-function artifact(n, h, a, d, minD, maxD, s)
+function Artifact(n, h, a, d, minD, maxD, s)
 {
 	this.name = n;
 	this.health = h;
@@ -8,12 +8,22 @@ function artifact(n, h, a, d, minD, maxD, s)
 	this.maxDmg = maxD;
 	this.speed = s;
 }
-var boots = new artifact('bootsOfSpeed', 20, 0, 5, 0, 0, 10);
-var sphere = new artifact('sphereOfLife', 100, 0, 0, 0, 0, 0);
-var hammer = new artifact('mithrilHammer', 0, 10, 0, 5, 5, 0);
-var armor = new artifact('heavyArmor', 50, 0, 20, 0, 0, -2);
+var boots = new Artifact('bootsOfSpeed', 20, 0, 5, 0, 0, 10);
+var sphere = new Artifact('sphereOfLife', 100, 0, 0, 0, 0, 0);
+var hammer = new Artifact('mithrilHammer', 0, 10, 0, 5, 5, 0);
+var armor = new Artifact('heavyArmor', 50, 0, 20, 0, 0, -2);
 
-function creature(art, n, h, a, d, minD, maxD, s)
+function stat(art, elem)
+{
+	var value = 0;
+	for(var i = 0; i < art.length; i++)
+	{
+		value += art[i][elem];
+	}
+	return value;
+}
+
+function Creature(art, n, h, a, d, minD, maxD, s)
 {
 	this.artifact = art;
 	this.name = n;
@@ -26,28 +36,27 @@ function creature(art, n, h, a, d, minD, maxD, s)
 	this.fullHP = h + stat(this.artifact, 'health');
 }
 
-function stat(art, elem)
+var knight = new Creature([armor, sphere], 'knight', 100, 80, 0, 10, 50, 8);
+var skeleton = new Creature([hammer], 'skeleton', 150, 50, 10, 5, 20, 10);
+var crusader = new Creature([boots], 'crusader', 500, 30, 50, 5, 10, 1);
+var griffon = new Creature([], 'griffon', 250, 80, 30, 15, 20, 9);
+
+function changeHPonDisplay(elem, h, f)
 {
-	var value = 0;
-	for(var i = 0; i < art.length; i++)
-	{
-		value += art[i][elem];
-	}
-	return value;
+	var el = document.getElementById(elem+'HP');
+	el.style['width'] = Math.trunc((h / f) * 100)+'px';
 }
 
-var knight = new creature([armor, sphere], 'knight', 100, 80, 0, 10, 50, 8);
-var skeleton = new creature([hammer], 'skeleton', 150, 50, 10, 5, 20, 10);
-var crusader = new creature([boots], 'crusader', 500, 30, 50, 5, 10, 1);
-var griffon = new creature([], 'griffon', 250, 80, 30, 15, 20, 9);
-
-creature.prototype.hit = function()
+Creature.prototype.hit = function(enemy)
 {
 	var randomDmg = this.minDmg + (this.maxDmg - this.minDmg) * Math.random();
 	var finalDmg = randomDmg + randomDmg * 0.01 * this.attack;
-	return finalDmg;
+	var damage = (finalDmg - finalDmg * 0.01 * enemy.defense).toFixed(1);
+	enemy.health -= damage;
+	changeHPonDisplay(enemy.name, enemy.health, enemy.fullHP);
+	return damage;
 }
-creature.prototype.info = function()
+Creature.prototype.checkHP = function()
 {
 	if(this.health > 0){return true;}
 	else{return false;}
@@ -63,15 +72,40 @@ allCreature.sort(function(a, b)
 });
 
 var enemy = [];
-var t1 = checkTeam(teamA);
-var t2 = checkTeam(teamB);
+
+
+function sendMessageAboutDeath(name)
+{
+	obj = document.getElementById(name);
+	str = '<span class="mes dead">' + name +' погибает</span>';
+	infoBox.innerHTML = str + infoBox.innerHTML;
+	obj.style['display'] = 'none';
+}
+function sendMessageAboutFight(x, y, d, color)
+{
+	str = '<span class="mes"><span class="'+color+'">'+x+'</span> атакует <span class="'+color+'">'+y+'</span> на <span class="'+color+'">'+d+'</span> урона</span>';
+	infoBox.innerHTML = str + infoBox.innerHTML;
+}
+
+function checkCountUnitInTeam(team)
+{
+	var count = 0;
+	team.forEach(function(elem)
+	{
+		if(elem.checkHP()){count++}
+	});
+	return count;
+}
+
+var t1 = checkCountUnitInTeam(teamA);
+var t2 = checkCountUnitInTeam(teamB);
 var i = 0;
 var infoBox = document.getElementById('infoBox');
 var str = '';
 var obj;
 function go()
 {
-	if(allCreature[i].info() == true)
+	if(allCreature[i].checkHP())
 	{
 		if(teamA.indexOf(allCreature[i]) != -1)
 		{
@@ -89,42 +123,34 @@ function go()
 		return;
 	}
 	var flag = false;
-	while(flag == false)
+	while(!flag)
 	{
 		var rand = Math.ceil(enemy.length*Math.random()) - 1;
 		var randomEnemy = enemy[rand];
-		if(randomEnemy.info() == true){flag = true;}
+		if(randomEnemy.checkHP()){flag = true;}
 	}
 
-	var creatureDmg = allCreature[i].hit();
-	var dmg = (creatureDmg - creatureDmg * 0.01 * randomEnemy.defense).toFixed(1);
-	randomEnemy.health -= dmg;
-
-	fight(allCreature[i].name, randomEnemy.name, dmg, 'red');
-	hp(randomEnemy.name, randomEnemy.health, randomEnemy.fullHP);
+	var dmg = allCreature[i].hit(randomEnemy);
+	sendMessageAboutFight(allCreature[i].name, randomEnemy.name, dmg, 'red');
 
 	setTimeout(function()
 	{
-		if(randomEnemy.info())
+		if(randomEnemy.checkHP())
 		{
-			var enemyDmg = randomEnemy.hit();
-			var reflectDmg = (enemyDmg - enemyDmg * 0.01 * allCreature[i].defense).toFixed(1);
-			allCreature[i].health -= reflectDmg;
+			var reflectDmg = randomEnemy.hit(allCreature[i])
+			sendMessageAboutFight(randomEnemy.name, allCreature[i].name, reflectDmg, 'green');
 
-			fight(randomEnemy.name, allCreature[i].name, reflectDmg, 'green');
-			hp(allCreature[i].name, allCreature[i].health, allCreature[i].fullHP);
-
-			if(allCreature[i].info() == false)
+			if(!allCreature[i].checkHP())
 			{
-				death(allCreature[i].name);
+				sendMessageAboutDeath(allCreature[i].name);
 			}
 		}
 		else
 		{
-			death(randomEnemy.name);
+			sendMessageAboutDeath(randomEnemy.name);
 		}
-		t1 = checkTeam(teamA);
-		t2 = checkTeam(teamB);
+		t1 = checkCountUnitInTeam(teamA);
+		t2 = checkCountUnitInTeam(teamB);
 		if(t1 == 0)
 		{
 			clearInterval(interval);
@@ -144,49 +170,19 @@ function go()
 
 var interval = setInterval(go, 4000);
 
-function death(name)
-{
-	obj = document.getElementById(name);
-	str = '<span class="mes dead">' + name +' погибает</span>';
-	infoBox.innerHTML = str + infoBox.innerHTML;
-	obj.style['display'] = 'none';
-}
-function fight(x, y, d, color)
-{
-	str = '<span class="mes"><span class="'+color+'">'+x+'</span> атакует <span class="'+color+'">'+y+'</span> на <span class="'+color+'">'+d+'</span> урона</span>';
-	infoBox.innerHTML = str + infoBox.innerHTML;
-}
-
-function hp (elem, h, f)
-{
-	var el = document.getElementById(elem+'HP');
-	el.style['width'] = Math.trunc((h / f) * 100)+'px';
-}
-
-function checkTeam(team)
-{
-	var count = 0;
-	team.forEach(function(elem)
-	{
-		if(elem.info()){count++}
-	});
-	return count;
-}
 var audio = document.getElementById("audio");
 var audioBut = document.getElementById("audioBut");
-var aud = true;
+
 audioBut.addEventListener('click', function()
 {
 	audioBut.classList.toggle('active');
-	if(aud)
+	if(audio.paused)
 	{
 		audio.play();
 		audio.volume = 0.3;
-		aud =!aud;
 	}
 	else
 	{
 		audio.pause();
-		aud =!aud;
 	}
 });
