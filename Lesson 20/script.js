676f519636602de64712b9f46e1c4026670718a4
Adrew Hod <andreihodunov3@tut.by>
2019-10-12 09:32:01 +0300

    Lesson 20(2)

--- Lesson 20/script.js
+++ Lesson 20/script.js
@@ -1,15 +1,14 @@
 var App = {
 	init() {
-		let swapforuser = false;
 		this.checkLogin();
-		this.EventHandlers();
+		this.EventHandlers.init();
 	}
 	,checkLogin() {
 		firebase.auth().onAuthStateChanged(function(e)
 		{
 			if(e)
 			{
-				ymaps.ready(App.loadMap);
+				ymaps.ready(App.loadMapWithPoints.loadMap());
 				$('#map').css('display', 'block')
 				$('.username').text(e.email);
 				$('.button-log').css('display', 'none');
@@ -22,106 +21,131 @@ var App = {
 			}
 		});
 	}
-	,EventHandlers() {
-		document.addEventListener('click', function(e)
-		{
-			const n = e.target.getAttribute('data-n');
-			if(e.target.classList.contains('change'))
+	,EventHandlers: {
+		
+		init(){
+			this.pointsWindow();
+			this.inputWindow();
+			this.swapSingAndRegistration();
+			this.sendDataOnSing();
+		},
+
+		pointsWindow(){
+			document.addEventListener('click', function(e)
 			{
 				const n = e.target.getAttribute('data-n');
-				$('.infoWindow').css('display', 'flex');
-				firebase.database().ref(`/${n}`).on('value', res =>
+				if(e.target.classList.contains('change'))
 				{
-				    $('.cord1').val(res.val().cord1);
-				    $('.cord2').val(res.val().cord2);
-				    $('.name').val(res.val().name);
-				    $('.description').val(res.val().description);
-				})
-			}
-			if(e.target.classList.contains('infoWindow-accept'))
+					const n = e.target.getAttribute('data-n');
+					$('.infoWindow-wrapper').show(200);
+					firebase.database().ref(`/${n}`).on('value', res =>
+					{
+					    $('.cord1').val(res.val().cord1);
+					    $('.cord2').val(res.val().cord2);
+					    $('.name').val(res.val().name);
+					    $('.description').val(res.val().description);
+					})
+				}
+				if(e.target.classList.contains('infoWindow-accept'))
+				{
+					firebase.database().ref(`/${n*1}`).update({
+					    cord1: $('.cord1').val(),
+					    cord2: $('.cord2').val(),
+					    description: $('.description').val(),
+					    name: $('.name').val()
+				  	});
+					location.reload();
+				}
+				if(e.target.classList.contains('infoWindow-exit'))
+				{
+					$('.infoWindow-wrapper').hide(200);
+				}
+			});			
+		},
+
+		inputWindow(){
+			$('.button-log').click(function(e)
 			{
-				firebase.database().ref(`/${n*1}`).update({
-				    cord1: $('.cord1').val(),
-				    cord2: $('.cord2').val(),
-				    description: $('.description').val(),
-				    name: $('.name').val()
-			  	});
-				location.reload();
-			}
-			if(e.target.classList.contains('infoWindow-exit'))
+				if($('.form-container-wrapper').css('display') == 'none')
+				{
+					$('.form-container-wrapper').css('display', 'flex');
+				}
+				else
+				{
+					$('.form-container-wrapper').css('display', 'none');
+				}
+			});			
+			$('.button-exit').click(function()
 			{
-				$('.infoWindow').css('display', 'none');
-			}
-		});
+				firebase.auth().signOut();
+				location.reload()
+			});
+		},
 
-		$('.button-log').click(function(e)
-		{
-			if(this.swapforuser)
+		swapSingAndRegistration(){
+			$('.button-registration').click(function()
 			{
-				$('.form-container-wrapper').css('display', 'none');
-			}
-			else
+				$('.button-singin').removeClass('active');
+				$('.button-registration').addClass('active');
+
+				$('#submitSingin').css('display', 'none');
+				$('#submitRegistration').css('display', 'block');
+			});
+
+			$('.button-singin').click(function()
 			{
-				$('.form-container-wrapper').css('display', 'flex');
-			}
-			this.swapforuser =! this.swapforuser;
-		});
+				$('.button-registration').removeClass('active');
+				$('.button-singin').addClass('active');
 
-		$('.button-registration').click(function()
-		{
-			$('.button-singin').css({background: '#8D8D8D', fontSize: '15px'});
-			$('.button-registration').css({background: '#555555', fontSize: '20px'});
-			$('#submitSingin').css('display', 'none');
-			$('#submitRegistration').css('display', 'block');
-		});
+				$('#submitSingin').css('display', 'block');
+				$('#submitRegistration').css('display', 'none');
+			});			
+		},
 
-		$('.button-singin').click(function()
-		{
-			$('.button-singin').css({background: '#555555', fontSize: '20px'});
-			$('.button-registration').css({background: '#8D8D8D', fontSize: '15px'});
-			$('#submitSingin').css('display', 'block');
-			$('#submitRegistration').css('display', 'none');
-		});
+		sendDataOnSing(){
+			$('#submitRegistration').click(function(e)
+			{
+				e.preventDefault();
+				const [$pass, $email] = [$('#password'), $('#email')]
+				const user = firebase.auth().createUserWithEmailAndPassword($email.val(), $pass.val());
+				user.then(()=> location.reload());
+				user.catch(function(error) {
+					alert(error.message)
+				});	
+			})
 
-		$('.button-exit').click(function()
-		{
-			firebase.auth().signOut();
-			location.reload()
-		});
+			$('#submitSingin').click(function(e)
+			{
+				e.preventDefault();
+				const [$pass, $email] = [$('#password'), $('#email')]
+				const user = firebase.auth().signInWithEmailAndPassword($email.val(), $pass.val());
+				user.then(()=> location.reload());
+				user.catch(function(error) {
+					alert(error.message)
+				});	
+			})			
+		}
+	},
 
-		$('#submitRegistration').click(function(e)
-		{
-			e.preventDefault();
-			const [$pass, $email] = [$('#password'), $('#email')]
-			const user = firebase.auth().createUserWithEmailAndPassword($email.val(), $pass.val());
-			user.then(()=> location.reload());
-			user.catch(function(error) {
-				alert(error.message)
-			});	
-		})
+	loadMapWithPoints: {
+		async loadPoints(){
+			return new Promise((resolve, reject) => {
+				var database = firebase.database();
+				database.ref('/').on('value', res => resolve(res.val()))
 
-		$('#submitSingin').click(function(e)
-		{
-			e.preventDefault();
-			const [$pass, $email] = [$('#password'), $('#email')]
-			const user = firebase.auth().signInWithEmailAndPassword($email.val(), $pass.val());
-			user.then(()=> location.reload());
-			user.catch(function(error) {
-				alert(error.message)
-			});	
-		})
+			})
+		},
 
-	}
-	,loadMap() {
-	    var myMap = new ymaps.Map("map", 
-	    {
-	        center: [53.896485, 27.547494],
-	        zoom: 12
-	    });
-		var database = firebase.database();
-		database.ref('/').on('value', res =>
-		{
-			res.val().forEach((e, index)=> 
+		async loadMap(){
+			var myMap = new ymaps.Map("map", 
+	    	{
+		        center: [53.896485, 27.547494],
+		        zoom: 12
+	    	});
+
+			const points = await this.loadPoints();
+
+			points.forEach((e, index)=> 
 			{
 				MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
 	            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
@@ -132,8 +156,8 @@ var App = {
 		            balloonContent: `<h3>${e.name}</h3><p class="desc">${e.description}</p><img class="map-image" src="${e.image}"/> <button class="change" data-n="${index}">Изменить</button>`
 		        }),
 				myMap.geoObjects.add(myPlacemark);
-			})
-		}) 
+			});
+		}
 	}
 }
 App.init();
